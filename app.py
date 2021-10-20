9399a9abf3ea951f986b2954e836caa670311c0b
AlexanderBowers <7319abowers@gmail.com>
2021-10-19 22:08:05 -0700

    added first paddle

--- app.py
+++ app.py
@@ -7,3 +7,14 @@ win.bgcolor("black")
 win.setup(width=800, height=600)
 win.tracer(0)
 
+# First Paddle
+paddle_a = turtle.Turtle()
+paddle_a.speed(0) 
+paddle_a.shape('square')
+paddle_a.color('white')
+paddle_a.shapesize(strech_wid=5, stretch_len=1)
+paddle_a.penup()
+paddle_a.goto(-350, 0)
+
+while True:
+    win.update()
