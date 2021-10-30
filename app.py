fbe947ef46f824894cb69747a30addd10c8f3b03
AlexanderBowers <7319abowers@gmail.com>
2021-10-30 15:48:25 -0700

    added ball

--- app.py
+++ app.py
@@ -25,5 +25,13 @@ paddle_b.shapesize(strech_wid=5, stretch_len=1)
 paddle_b.penup()
 paddle_b.goto(350, 0)
 
+# Ball
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape("square")
+ball.color("white")
+ball.penup()
+ball.goto(0, 0)
+
 while True:
     win.update()
